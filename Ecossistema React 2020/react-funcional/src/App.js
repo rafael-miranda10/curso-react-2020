c721f181346f4df48d29d79e6866537bfb7644f9
Rafael Miranda <rafael.miranda@db1.com.br>
2020-03-10 17:42:25 -0300

    [DEV] useState

--- Ecossistema React 2020/react-funcional/src/App.js
+++ Ecossistema React 2020/react-funcional/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [numero, setNumero] = useState();
+  const [sehundoNumero, setSegundoNumero] = useState();
+  const [resultado, setResultado] = useState();
+
+  const [state, setState] = useState({
+    numero: 0,
+    sehundoNumero: 0,
+    resultado: 0
+  });
+
+  setNumero(10);
+
   return (
     <div>
-
+         <input type="text" value={numero}/><br/>
+         <input type="text" value={sehundoNumero}/><br/>
+         <input type="text" value={resultado}/><br/>
     </div>
   );
 }
